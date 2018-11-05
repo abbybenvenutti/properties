b783553ad23c0f3b41dddf6820edef22cc9cf141
Abby Benvenutti <abbybenvenutti@gmail.com>
2018-11-05 10:49:50 -0500

    fixed secrets

--- client/components/Owner.js
+++ client/components/Owner.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 const data = require('../../secrets');
 
+
 const Owner = (props) => {
-	let current = props.data;
+  let current = props.data;
+  let devices = []
 
 	for (let person of data.people) {
 		if (person['first_name'] === current.firstName && person['last_name'] === current.lastName) {
+
+      if(person["roles"].indexOf("Admin") !== -1){
+
+        // for(let item of data.devices) {
+          // if((data.devices["unit"] === props.data.unit) && person["admin_accessible"]) {
+          //   devices.push(data.devices.model)
+          //   console.log("data", devices)
+          // }
+        }
+      // }
 			return (
 				<ul>
 					<li>Name: {person['first_name'] + ' ' + person['last_name']}</li>
 					<li>Unit: {person.unit}</li>
 					<li>Role(s): {person.roles.join(', ')}</li>
+          <li>Devices:</li>
 				</ul>
+
 			);
 		}
 	}
